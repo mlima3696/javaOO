0c04156ddd0c40f42f318d2e9414a971b1d48e66
mlima3696 <mjuniorlima3@gmail.com>
2020-05-29 19:18:25 -0300

    Thread em objeto e o Runnable

--- src/cursojava/thread/AulaThread.java
+++ src/cursojava/thread/AulaThread.java
@@ -6,61 +6,68 @@ public class AulaThread {
 	public static void main(String[] args) {
 
 		//Thread processando em paralelo do envio de email
-		new Thread() {
-
-			public void run() {// Executa o que nos queremos
-				// Codigo da rotina
-
-				// Codigo da rotina que eu quero executar em paralelo
-				for (int pos = 0; pos < 10; pos++) {
-
-					// Quero executar esse envio com unmtempo de parada, ou tempo determinado
-					System.out.println("Executando uma rotina, envio de email");
-
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				} // Fim do codigo paralelo
-
-			};
-
-		}.start();// Start liga a thread que fica processando paralelamente por tras
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 		
-		//************************************************************************************************
+		
 		
 		//Thread processando em paralelo do envio de nota fiscal
-				new Thread() {
+				Thread threadNFCE = new Thread(thread2);
+				threadNFCE.start();
+				
+				// Fluxo do sistema
+				System.out.println("Chegou ao fim do codigo de teste de Thread");
 
-					public void run() {// Executa o que nos queremos
-						// Codigo da rotina
+				JOptionPane.showMessageDialog(null, "Sistema continua executando");//Fica null porque nao tem nenhum objeto Pai
+																					 
+			}
+	
+	private static Runnable thread2 = new Runnable() {
+		
+		@Override
+		public void run() {
+			
+			// Codigo da rotina que eu quero executar em paralelo
+			for (int pos = 0; pos < 10; pos++) {
 
-						// Codigo da rotina que eu quero executar em paralelo
-						for (int pos = 0; pos < 10; pos++) {
+				// Quero executar esse envio com unmtempo de parada, ou tempo determinado
+				System.out.println("Executando uma rotina, envio de nota fiscal");
 
-							// Quero executar esse envio com unmtempo de parada, ou tempo determinado
-							System.out.println("Executando uma rotina, envio de nota fiscal");
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
-							try {
-								Thread.sleep(1000);
-							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
+			} // Fim do codigo paralelo
+			
+		}
+	};
+	
+			private static Runnable thread1 = new Runnable() {
 
-						} // Fim do codigo paralelo
+				@Override
+				public void run() {
+					
+					// Codigo da rotina
 
-					};
+					// Codigo da rotina que eu quero executar em paralelo
+					for (int pos = 0; pos < 10; pos++) {
 
-				}.start();// Start liga a thread que fica processando paralelamente por tras
+						// Quero executar esse envio com unmtempo de parada, ou tempo determinado
+						System.out.println("Executando uma rotina, envio de email");
 
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 
-		// Fluxo do sistema
+					} // Fim do codigo paralelo
 
-		JOptionPane.showMessageDialog(null, "Sistema continua executando");// Fica null porque nao tem nenhum objeto Pai
-		System.out.println("Chegou ao fim do codigo de teste de Thread");
-	}
-}
+				}
+			};
+
+		}
